e3bf0cc142403180deeff9c141924a726c5f52d9
Alejandro De Jesus <z@Alejandros-Air.fibertel.com.ar>
2020-02-09 04:50:13 -0300

    final

---
+++ bitcoinAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitcoinAPI
+//
+//  Created by Alejandro De Jesus on 09/02/2020.
+//  Copyright © 2020 Alejandro De Jesus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bitcoinAPI/model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  bitcoinAPI
+//
+//  Created by Alejandro De Jesus on 09/02/2020.
+//  Copyright © 2020 Alejandro De Jesus. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData: Decodable {
+    let rate: Double
+}
